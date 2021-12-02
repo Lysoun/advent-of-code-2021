3cf68d89f200b4e1aff8a655fbab84d04dea3321
Élise Rubio <elise.rubio@4sh.fr>
2021-12-02 07:52:18 +0100

    :sparkles: Day 2 part 2 solution

--- day2/part2.ml
+++ day2/part2.ml
@@ -4,18 +4,27 @@ open List
 
 let input = "input.txt"
 
-let move instruction units (x,y) = match instruction with
-    | "forward" -> (x + units, y)
-    | "up" -> (x, y - units)
-    | _ -> (x, y + units)
+let compute_new_aim instruction units aim = match instruction with
+    | "down" -> aim + units
+    | "up" -> aim - units
+    | _ -> aim
 ;;
 
-let rec input_lines file (x, y) =
+let move_submarine instruction units (x,y) aim = match instruction with
+    | "forward" -> (x + units, y + units * aim)
+    | _ -> (x, y)
+;;
+
+let rec input_lines file (x, y) aim =
    match try (input_line file) with End_of_file -> "" with
       | "" -> x * y
       | line ->
         let splittedLine = (String.split_on_char ' ' line) in
-        (input_lines file (move (List.nth splittedLine 0) (int_of_string (List.nth splittedLine 1)) (x, y)))
+        let instruction = (List.nth splittedLine 0) in
+        let units = (int_of_string (List.nth splittedLine 1)) in
+        if (instruction = "forward")
+        then (input_lines file (move_submarine instruction units (x, y) aim) aim)
+        else (input_lines file (x, y) (compute_new_aim instruction units aim))
     ;;
 
-print_int (input_lines (open_in input) (0, 0));;
+print_int (input_lines (open_in input) (0, 0) 0);;
