1a157c0666437600a13c233eb1f73670138166b5
Élise Rubio <elise.rubio@4sh.fr>
2021-12-05 12:48:18 +0100

    :sparkles: Day 5 part 1 solution

--- day5/part1.ml
+++ day5/part1.ml
@@ -1,11 +1,50 @@
 open Printf
 
-let input = "input.txt"
+let input = "input.txt";;
 
-let rec input_lines file =
+let count_overlapping_points vents =
+    (Seq.fold_left (fun sum ventNumber -> sum + 1) 0 (Seq.filter (fun ventNumber -> ventNumber > 1) (Hashtbl.to_seq_values vents)))
+;;
+
+let rec range a b =
+    if(b < a)
+    then (range b a)
+    else (List.init (b - a + 1) (fun index -> a + index))
+;;
+
+let coordinates_to_range (x1, y1) (x2, y2) =
+    if(x1 <> x2 && y1 <> y2)
+    then []
+    else
+        if(x2 <> x1)
+        then (List.map (fun x -> (x, y1)) (range x1 x2))
+        else (List.map (fun y -> (x1, y)) (range y1 y2))
+;;
+
+let string_to_coordinates stringCoordinates =
+    let coordinatesList = (List.map (int_of_string) (String.split_on_char ',' stringCoordinates)) in
+    ((List.nth coordinatesList 0), (List.nth coordinatesList 1))
+;;
+
+let line_to_coordinates line =
+    let splittedLine = (String.split_on_char ' ' line) in
+    ((string_to_coordinates (List.nth splittedLine 0)), (string_to_coordinates (List.nth splittedLine 2)))
+;;
+
+let add_vent ventPosition vents =
+    if (Hashtbl.mem vents ventPosition)
+    then Hashtbl.replace vents ventPosition ((Hashtbl.find vents ventPosition) + 1)
+    else Hashtbl.add vents ventPosition 1
+;;
+
+
+let rec input_lines file vents =
    match try (input_line file) with End_of_file -> "" with
-      | "" -> 0
-      | line -> (input_lines file)
+      | "" -> (count_overlapping_points vents)
+      | line -> let (p1, p2) = (line_to_coordinates line) in
+        let lineVent = (coordinates_to_range p1 p2) in
+        (List.iter (fun ventPosition -> (add_vent ventPosition vents)) lineVent);
+        (input_lines file vents)
     ;;
 
-print_int (input_lines (open_in input));;
+print_int (input_lines (open_in input) (Hashtbl.create 500));;
