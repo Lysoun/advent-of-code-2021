8183f78d87bf4f95552c5af24a2baebbadc25cc5
Élise Rubio <elise.rubio@4sh.fr>
2021-12-10 08:39:55 +0100

    :sparkles: Day 10 part 1

--- day10/part1.ml
+++ day10/part1.ml
@@ -2,10 +2,44 @@ open Printf
 
 let input = "input.txt"
 
-let rec input_lines file =
+let is_opening_character character =
+    (List.fold_left (fun result openingCharacter -> result || (character =openingCharacter)) false ['('; '['; '<'; '{'])
+;;
+
+let corresponding_closing_character openingCharacter = match openingCharacter with
+    | '(' -> ')'
+    | '[' -> ']'
+    | '<' -> '>'
+    | _ -> '}'
+;;
+
+let is_corresponding_closing_character character openingCharacter = character = (corresponding_closing_character openingCharacter);;
+
+let illegal_character_score illegalCharacter = match illegalCharacter with
+  | ')' -> 3
+  | ']' -> 57
+  | '>' -> 25137
+  | _ -> 1197
+;;
+
+let compute_corrupted_line_score line =
+    let rec aux charactersList openingCharacters = match charactersList with
+        | [] -> 0
+        | h::t when (is_opening_character h) -> (aux t (h::openingCharacters))
+        | h::t ->
+            if(is_corresponding_closing_character h (List.hd openingCharacters))
+            then (aux t (List.tl openingCharacters))
+            else (illegal_character_score h)
+    in
+
+    let charactersList =  (List.of_seq (String.to_seq line)) in
+    (aux charactersList [])
+;;
+
+let rec input_lines file illegalCharactersScore =
    match try (input_line file) with End_of_file -> "" with
-      | "" -> 0
-      | line -> (input_lines file)
+      | "" -> illegalCharactersScore
+      | line -> (input_lines file (illegalCharactersScore + (compute_corrupted_line_score line)))
     ;;
 
-print_int (input_lines (open_in input));;
+print_int (input_lines (open_in input) 0);;
